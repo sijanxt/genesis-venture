f75d73b210001b3c053830eeba091d464c57c483
sijanxt <sijanbartaula39@gmail.com>
2026-04-21 18:23:52 +0545

    fix:changes

--- app/relation/page.tsx
+++ app/relation/page.tsx
@@ -1,19 +1,20 @@
 import Hero from "@/components/InvestorRelations/Hero";
-import InvestorDocuments from "@/components/InvestorRelations/form";
 import ReportsAndUpdates from "@/components/InvestorRelations/ReportsAndUpdates";
+import PartnerCTA from "@/components/InvestorRelations/commitment";
+import MarketOverlook from "@/components/InvestorRelations/MarketOverlook";
 import HeroPage from "@/components/ui/HeroPage";
 
 export default function Home() {
   return (
-    <main className="w-full font-[PPFONT]">
+    <main className="w-full bg-white text-[#2c2c34] font-poppins overflow-x-hidden">
       <HeroPage
         title="Investor Relations"
         titleClassName="text-[clamp(1.5rem,4.8vw,3.2rem)]"
       />
       <Hero />
       <ReportsAndUpdates />
-      <InvestorDocuments />
-      {/* <PartnerCTA /> */}
+      <PartnerCTA />
+      <MarketOverlook />
     </main>
   );
 }
